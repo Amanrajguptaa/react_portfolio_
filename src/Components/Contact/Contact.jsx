bf00161c97cbbe3c0be9c98f61ac30dfd2518cdd
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-03-01 01:19:05 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,12 +7,12 @@ export default function Contact(){
             <p class="mt-5">Let's bridge the digital expanse together - reach out, connect, and let's create something extraordinary!</p>
             <div class="mt-20 flex items-center justify-center gap-20 flex-wrap pb-5">
                 <div class="flex justify-center items-center">
-                    <a href="#"><img class="p-5" src="/src/media/phone_logo.png" alt=""/></a>
+                    <a href="#"><img class="p-5" src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/phone_logo_lvdqlm.png" alt=""/></a>
                     <p>+91-9262734111</p>
                 </div>
-                    <a href="https://www.linkedin.com/in/aman-raj-gupta-/" target="_blank"><img class="p-5" src="/src/media/linkedin_logo.png" alt=""/></a>
-                    <a href="https://github.com/Amanrajguptaa" target="_blank"><img class="p-5" src="/src/media/github_logo.png" alt=""/></a>
-                    <a href="mailto:rajaman9355@gmail.com"><img class="p-5" src="/src/media/gmail_logo.png" alt=""/></a>
+                    <a href="https://www.linkedin.com/in/aman-raj-gupta-/" target="_blank"><img class="p-5" src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/linkedin_logo_uac6l6.png" alt=""/></a>
+                    <a href="https://github.com/Amanrajguptaa" target="_blank"><img class="p-5" src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/github_logo_ky7tbr.png" alt=""/></a>
+                    <a href="mailto:rajaman9355@gmail.com"><img class="p-5" src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/gmail_logo_mpfose.png" alt=""/></a>
             </div>
         </div>
         </section>

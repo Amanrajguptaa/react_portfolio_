ac6827d070f8c2d7252855e07a4624d565eec7b7
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-03-01 01:24:55 +0530

    Update Projects.jsx

--- src/Components/Project/Projects.jsx
+++ src/Components/Project/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects(){
             <div><h3 class="text-4xl font-bold">PROJECTS</h3></div>
             <div class="flex justify-between items-center flex-wrap gap-20">
                 <div class="transition-all duration-400 hover:scale-110 h-96 w-80 p-2 flex flex-col justify-between shadow-lg shadow-blue-500 border border-blue-400 rounded-lg"> 
-                    <img src="/src/media/amazonclone.png" alt=""/>
+                    <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/amazonclone_gejg1l.png" alt=""/>
                     <div class="flex flex-col justify-center items-center">
                 <p class=" text-xl font-bold">Amazon UI Clone</p>
                         <p class="text-sm">Technologies use:- HTML for making the structure and CSS for styling the user interface</p>
@@ -17,7 +17,7 @@ export default function Projects(){
                     </div>
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 h-96 w-80 p-2 flex flex-col justify-between shadow-lg shadow-blue-500 border border-blue-400 rounded-lg"> 
-                <img src="/src/media/tic-tac-toe.png" alt=""/>
+                <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235941/media/Tic-Tac-Toe_v3ii34.png" alt=""/>
                     <div class="flex flex-col justify-center items-center">
                         <p class=" text-xl font-bold">TIC-TAC-TOE GAME</p>
                         <p class="text-sm">Technologies use:- HTML for making the structure and CSS for styling and JAVASCRIPT for defining the logic for the activity.</p>
@@ -28,7 +28,7 @@ export default function Projects(){
                     </div>
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 h-96 w-80 p-2 flex flex-col justify-between shadow-lg shadow-blue-500 border border-blue-400 rounded-lg"> 
-                <img src="/src/media/amazonclone.png" alt=""/>
+                <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/amazonclone_gejg1l.png" alt=""/>
                     <p class="font-bold">Amazon UI Clone</p>
                     <p>Technologies use:- HTML for making the structure and CSS for styling the user interface</p>
                     <div class="flex  items-center justify-evenly">
@@ -37,7 +37,7 @@ export default function Projects(){
                     </div>
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 h-96 w-80 p-2 flex flex-col justify-between shadow-lg shadow-blue-500 border border-blue-400 rounded-lg"> 
-                <img src="/src/media/amazonclone.png" alt=""/>
+                <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/amazonclone_gejg1l.png" alt=""/>
                     <p class="font-bold">Amazon UI Clone</p>
                     <p>Technologies use:- HTML for making the structure and CSS for styling the user interface</p>
                     <div class="flex  items-center justify-evenly">
@@ -46,7 +46,7 @@ export default function Projects(){
                     </div>
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 h-96 w-80 p-2 flex flex-col justify-between shadow-lg shadow-blue-500 border border-blue-400 rounded-lg"> 
-                <img src="/src/media/amazonclone.png" alt=""/>
+                <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/amazonclone_gejg1l.png" alt=""/>
                     <p class="font-bold">Amazon UI Clone</p>
                     <p>Technologies use:- HTML for making the structure and CSS for styling the user interface</p>
                     <div class="flex  items-center justify-evenly">
@@ -55,7 +55,7 @@ export default function Projects(){
                     </div>
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 h-96 w-80 p-2 flex flex-col justify-between shadow-lg shadow-blue-500 border border-blue-400 rounded-lg"> 
-                <img src="/src/media/amazonclone.png" alt=""/>
+                <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/amazonclone_gejg1l.png" alt=""/>
                     <p class="font-bold">Amazon UI Clone</p>
                     <p>Technologies use:- HTML for making the structure and CSS for styling the user interface</p>
                     <div class="flex  items-center justify-evenly">

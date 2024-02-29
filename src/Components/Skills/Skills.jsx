d8bd6b15ea9b3e8ba964b160b9ceb19ea9fa341e
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-03-01 01:28:17 +0530

    Update Skills.jsx

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -6,39 +6,39 @@ export default function Skills(){
             <div class="flex justify-center text-4xl font-bold"><h2>SKILLS</h2></div>
             <div class="flex items-center justify-center gap-10 flex-wrap p-10 ">
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/html_log.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/html_log_p7ytjd.png" alt=""/>    
                     <h5>HTML</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="src/media/c_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/css_logo_oxobxs.png" alt=""/>    
                     <h5 >CSS</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img src="/src/media/tailwind_logo.png" alt=""/>    
+                    <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/tailwind_logo_dfbg2j.png" alt=""/>    
                     <h5>TAILWIND</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/js_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/js_logo_ydv2de.png" alt=""/>    
                     <h5>JAVASCRIPT</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/react_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/react_logo_yqparm.png" alt=""/>    
                     <h5>REACT</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/c_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/c_logo_nik7jd.png" alt=""/>    
                     <h5>C LANGUAGE</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/python_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/python_logo_b8pwn5.png" alt=""/>    
                     <h5>PYTHON</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/excel_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/excel_logo_xefsil.png" alt=""/>    
                     <h5 >EXCEL</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
-                    <img  src="/src/media/canva_logo.png" alt=""/>    
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/canva_logo_ni5kns.png" alt=""/>    
                     <h5 >CANVA</h5>    
                 </div>
             </div>

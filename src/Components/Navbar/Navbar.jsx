2eaba752f6444b36a01e1df1964e8f37e0c48aa5
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-03-01 01:20:29 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar()
         <>
         <nav class="max-w-full h-14 bg-zinc-900">
         <div class="flex justify-between items-center px-10">
-            <img class="h-14 " src="/src/media/logo.png"alt="logo"/>
+            <img class="h-14 " src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/logo_gorqjb.png"alt="logo"/>
             <ul class="hidden md:flex mr-5 items-center gap-10 text-white justify-center font-bold">
                 <li className="hover:border border-white p-2"><HomeIcon className='text-large'/></li>
                 <li className="hover:border border-white p-2">About me</li> 

9d8add3b10f0148c5605262cc358339965315626
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-03-11 10:39:43 +0530

    Update Skills.jsx

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -34,6 +34,10 @@ export default function Skills(){
                     <h5>PYTHON</h5>    
                 </div>
                 <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                    <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1710133728/media/favicon_white-1__1_-removebg-preview_1_mqpiom.png" alt=""/>    
+                    <h5>PANDAS</h5>    
+                </div>
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1710133426/media/sql-1__1_-removebg-preview_1_1_zxligu.png" alt=""/>    
                     <h5 >SQL</h5>    
                 </div>

81cae2d17f14758031b07883767bdaf07c15615f
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-03-11 10:43:08 +0530

    Update Skills.jsx

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -5,47 +5,47 @@ export default function Skills(){
         <div class="my-20 px-10 flex-col items-center justify-between">
             <div class="flex justify-center text-4xl font-bold"><h2>SKILLS</h2></div>
             <div class="flex items-center justify-center gap-10 flex-wrap p-10 ">
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/html_log_p7ytjd.png" alt=""/>    
                     <h5>HTML</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/css_logo_oxobxs.png" alt=""/>    
                     <h5 >CSS</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/tailwind_logo_dfbg2j.png" alt=""/>    
                     <h5>TAILWIND</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/js_logo_ydv2de.png" alt=""/>    
                     <h5>JAVASCRIPT</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/react_logo_yqparm.png" alt=""/>    
                     <h5>REACT</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/c_logo_nik7jd.png" alt=""/>    
                     <h5>C LANGUAGE</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235940/media/python_logo_b8pwn5.png" alt=""/>    
                     <h5>PYTHON</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1710133728/media/favicon_white-1__1_-removebg-preview_1_mqpiom.png" alt=""/>    
                     <h5>PANDAS</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1710133426/media/sql-1__1_-removebg-preview_1_1_zxligu.png" alt=""/>    
                     <h5 >SQL</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/excel_logo_xefsil.png" alt=""/>    
                     <h5 >EXCEL</h5>    
                 </div>
-                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center shadow-lg shadow-blue-500 rounded-md">
+                <div class="transition-all duration-400 hover:scale-110 relative h-32 w-32 p-4 flex flex-col justify-between items-center border border-blue-600 shadow-lg shadow-blue-500 rounded-md">
                     <img  src="https://res.cloudinary.com/dkqbawsqm/image/upload/v1709235939/media/canva_logo_ni5kns.png" alt=""/>    
                     <h5 >CANVA</h5>    
                 </div>
